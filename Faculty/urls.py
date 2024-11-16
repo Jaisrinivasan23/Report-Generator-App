6fd47f93038a8e78011847f34d4be0ea63c3eba2
jaiisrinivasan23 <jaiisrinivasan2305@gmail.com>
2024-11-16 21:17:31 +0530

    Add media URL configuration and update urlpatterns for serving media files in debug mode

--- Faculty/urls.py
+++ Faculty/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', views.faculty_dashboard, name='faculty_dashboard'),
     path('add_event/', views.add_event, name='add_event'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
